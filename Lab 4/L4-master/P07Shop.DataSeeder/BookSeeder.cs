8a31260873b461366523d3cff09f4de2919d0c6f
Kamilj-wawa02 <95346542+Kamilj-wawa02@users.noreply.github.com>
2023-11-01 23:57:22 +0100

    Update BookSeeder.cs

--- Lab 4/L4-master/P07Shop.DataSeeder/BookSeeder.cs
+++ Lab 4/L4-master/P07Shop.DataSeeder/BookSeeder.cs
@@ -9,6 +9,7 @@ namespace P07Shop.DataSeeder
         {
             int productId = 1;
             var productFaker = new Faker<Book>()
+                .UseSeed(123456)
                 .RuleFor(x => x.Title, x => x.Commerce.ProductName())
                 .RuleFor(x => x.Author, x => x.Name.FullName())
                 .RuleFor(x => x.Description, x => x.Commerce.ProductDescription())
