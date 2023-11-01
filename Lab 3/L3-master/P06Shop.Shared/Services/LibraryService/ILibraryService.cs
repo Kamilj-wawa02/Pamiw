4cdca21d59b16c5d0b0a6e25cfecf1ef49231549
Kamilj-wawa02 <95346542+Kamilj-wawa02@users.noreply.github.com>
2023-11-01 20:58:21 +0100

    Add files via upload

---
+++ Lab 3/L3-master/P06Shop.Shared/Services/LibraryService/ILibraryService.cs
@@ -0,0 +1,9 @@
+﻿using P06Shop.Shared.Library;
+
+namespace P06Shop.Shared.Services.LibraryService
+{
+    public interface ILibraryService
+    {
+        Task<ServiceResponse<List<Book>>> GetBooksAsync();
+    }
+}
---
+++ Lab 3/L3-master/P07Shop.DataSeeder/BookSeeder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using P06Shop.Shared.Library;
+
+namespace P07Shop.DataSeeder
+{
+    public class BookSeeder
+    {
+        public static List<Book> GenerateBookData()
+        {
+            int productId = 1;
+            var productFaker = new Faker<Book>()
+                .RuleFor(x=>x.Title, x=>x.Commerce.ProductName())
+                .RuleFor(x => x.Author, x => x.Commerce.ProductName())
+                .RuleFor(x => x.Description, x => x.Commerce.ProductDescription())
+                .RuleFor(x=>x.Id, x=> productId++);
+
+            return productFaker.Generate(10).ToList();
+
+        }
+    }
+}
