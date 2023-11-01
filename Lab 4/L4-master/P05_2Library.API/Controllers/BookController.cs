764dbbf5de67896a25a4d4aa642331b7e62bf253
Kamilj-wawa02 <95346542+Kamilj-wawa02@users.noreply.github.com>
2023-11-01 21:03:26 +0100

    Add files via upload

---
+++ Lab 4/L4-master/P05_2Library.API/Controllers/BookController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using P06Shop.Shared;
+using P06Shop.Shared.Services.LibraryService;
+using P06Shop.Shared.Library;
+
+namespace P05Shop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ILibraryService _libraryService;
+        private readonly ILogger<BookController> _logger;
+        public BookController(ILibraryService productService, ILogger<BookController> logger)
+        {
+            _libraryService = productService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Book>>>> GetBooks()
+        {
+            _logger.Log(LogLevel.Information, "Invoked GetBooks Method in controller");
+
+            var result = await _libraryService.GetBooksAsync();
+
+            if (result.Success)
+                return Ok(result);
+            else
+                return  StatusCode(500, $"Internal server error {result.Message}");
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<Book>>> UpdateBook([FromBody] Book product)
+        {
+            
+            var result = await _libraryService.UpdateBookAsync(product);
+
+            if (result.Success)
+                return Ok(result);
+            else
+                return StatusCode(500, $"Internal server error {result.Message}");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<Book>>> CreateBook([FromBody] Book product)
+        {
+            var result = await _libraryService.CreateBookAsync(product);
+
+            if (result.Success)
+                return Ok(result);
+            else
+                return StatusCode(500, $"Internal server error {result.Message}");
+        }
+
+        // http:localhost/api/product/delete?id=4
+        // http:localhost/api/product/4 dla api REST
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<bool>>> DeleteBook([FromRoute] int id)
+        {
+            var result = await _libraryService.DeleteBookAsync(id);
+
+            if (result.Success)
+                return Ok(result);
+            else
+                return StatusCode(500, $"Internal server error {result.Message}");
+        }
+
+
+
+    }
+}
---
+++ Lab 4/L4-master/P05_2Library.API/Services/LibraryService/LibraryService.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P05Shop.API.Models;
+using P06Shop.Shared;
+using P06Shop.Shared.Services.LibraryService;
+using P06Shop.Shared.Library;
+using P07Shop.DataSeeder;
+
+namespace P05Shop.API.Services.LibraryService
+{
+    public class LibraryService : ILibraryService
+    {
+        private readonly DataContext _dataContext;
+        public LibraryService(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        public async Task<ServiceResponse<Book>> CreateBookAsync(Book book)
+        {
+            try
+            {
+                _dataContext.Books.Add(book);
+                await _dataContext.SaveChangesAsync();
+                return new ServiceResponse<Book>() { Data = book, Success = true };
+            }
+            catch (Exception)
+            {
+                return new ServiceResponse<Book>()
+                {
+                    Data = null,
+                    Success = false,
+                    Message = "Cannot create book"
+                };
+            }
+          
+        }
+
+        public async Task<ServiceResponse<bool>> DeleteBookAsync(int id)
+        {
+            var book = new Book() { Id = id };
+            _dataContext.Books.Attach(book);
+            _dataContext.Books.Remove(book);
+            await _dataContext.SaveChangesAsync();
+
+            return new ServiceResponse<bool>() {  Data = true, Success = true };
+        }
+
+        public async Task<ServiceResponse<List<Book>>> GetBooksAsync()
+        {
+            var books = await _dataContext.Books.ToListAsync();
+
+            try
+            {
+                var response = new ServiceResponse<List<Book>>()
+                {
+                    Data = books,
+                    Message = "Ok",
+                    Success = true
+                };
+
+                return response;
+            }
+            catch (Exception)
+            {
+                return new  ServiceResponse<List<Book>>()
+                {
+                    Data = null,
+                    Message = "Problem with database",
+                    Success = false
+                };
+            }
+           
+        }
+
+        public async Task<ServiceResponse<Book>> UpdateBookAsync(Book book)
+        {
+            try
+            {
+                var bookToEdit = new Book() { Id = book.Id };
+                _dataContext.Books.Attach(bookToEdit);
+
+                bookToEdit.Title = book.Title;
+                bookToEdit.Description = book.Description;
+                bookToEdit.Price = book.Price;
+                bookToEdit.Barcode = book.Barcode;
+                bookToEdit.ReleaseDate = book.ReleaseDate;
+
+                await _dataContext.SaveChangesAsync();
+                return new ServiceResponse<Book> { Data = bookToEdit, Success = true };
+            }
+            catch (Exception)
+            {
+                return new ServiceResponse<Book>
+                {
+                    Data = null,
+                    Success = false,
+                    Message = "An error occured while updating book"
+                };
+            }
+            
+        }
+    }
+}
