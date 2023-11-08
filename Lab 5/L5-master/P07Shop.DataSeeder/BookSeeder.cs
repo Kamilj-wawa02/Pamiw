c52a848509968553588a5e9272220cb76872cb57
Kamilj-wawa02 <95346542+Kamilj-wawa02@users.noreply.github.com>
2023-11-08 14:39:55 +0100

    Add files via upload

---
+++ Lab 5/L5-master/P07Shop.DataSeeder/BookSeeder.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+using P06Shop.Shared.Library;
+
+namespace P07Shop.DataSeeder
+{
+    public class BookSeeder
+    {
+        public static List<Book> GenerateBookData()
+        {
+            int productId = 1;
+            var productFaker = new Faker<Book>()
+                .UseSeed(123456)
+                .RuleFor(x => x.Title, x => x.Commerce.ProductName())
+                .RuleFor(x => x.Author, x => x.Name.FullName())
+                .RuleFor(x => x.Description, x => x.Commerce.ProductDescription())
+                .RuleFor(x => x.Id, x => productId++)
+                .RuleFor(x => x.Barcode, x => x.Commerce.Ean13().Substring(12))
+                .RuleFor(x => x.Price, x => x.Random.Double(1, 1000))
+                .RuleFor(x => x.ReleaseDate, x => x.Date.Past());
+
+            return productFaker.Generate(50).ToList();
+        }
+    }
+}
