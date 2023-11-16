fcddbad7706cdd2d80f3b87fd2adece749640cc1
Kamilj-wawa02 <95346542+Kamilj-wawa02@users.noreply.github.com>
2023-11-17 00:53:11 +0100

    Add files via upload

---
+++ Lab 6/L6-master/P06Shop.Shared/Services/LibraryService/ILibraryService.cs
@@ -0,0 +1,19 @@
+﻿using P06Shop.Shared;
+using P06Shop.Shared.Library;
+
+namespace P06Shop.Shared.Services.LibraryService
+{
+    public interface ILibraryService
+    {
+        Task<ServiceResponse<List<Book>>> GetBooksAsync();
+
+        Task<ServiceResponse<Book>> UpdateBookAsync(Book product);
+
+        Task<ServiceResponse<bool>> DeleteBookAsync(int id);
+
+        Task<ServiceResponse<Book>> CreateBookAsync(Book product);
+
+        Task<ServiceResponse<Book>> GetBookByIdAsync(int id);
+        Task<ServiceResponse<List<Book>>> SearchBooksAsync(string text, int page, int pageSize);
+    }
+}
